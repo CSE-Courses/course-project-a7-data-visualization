80586f4e4b3ffb7c564bcf86283e377686b45c80
HueyT <rhthurmo@buffalo.edu>
2020-10-18 18:43:38 -0400

    #55 Updated drop down menus for region/platform choice

--- src/server.py
+++ src/server.py
@@ -21,41 +21,17 @@ def returnhome():
 def leagueOne():
     return render_template("league1.html")
 
-@app.route("/league2.html")
-def leagueTwo():
-    return render_template("league2.html")
-
-@app.route("/league2_1.html")
-def leagueTwo_1():
-    return render_template("league2_1.html")
-
-@app.route("/league2_1.html", methods=['POST'])
+@app.route("/league1.html", methods=['POST'])
 def league():
-    username = request.form["Search"]
-    region = 'na1'
+    gamertag = request.form["Gamertag"]
+    region = request.form["Region"]
     df = pd.DataFrame()
-    df = loldata.getLolData(username, region)
+    df = loldata.getLolData(gamertag, region)
     return render_template("leagueOfLegendView.html", name='League Of legends Player Data', data=df.to_html())
 
 @app.route("/cod1.html")
 def codOne():
     return render_template("cod1.html")
-
-@app.route("/cod2.html")
-def codTwo():
-    return render_template("cod2.html")
-
-@app.route("/cod2___1.html")
-def codTwo_one():
-    return render_template("cod2___1.html")
-
-@app.route("/cod2___2.html")
-def codTwo_Two():
-    return render_template("cod2___2.html")
-
-@app.route("/cod2___3.html")
-def codTwo_Three():
-    return render_template("cod2___3.html")
 
 
 """

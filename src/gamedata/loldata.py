e76834809e57f8c04f59726c00d752318648c5ce
rishit <rishitpa@buffalo.edu>
2020-10-24 12:09:38 +0530

    Added : the image url for the champion, item 2-4, item names, spell names.

--- src/gamedata/loldata.py
+++ src/gamedata/loldata.py
@@ -4,7 +4,7 @@ from riotwatcher import LolWatcher
 
 
 def getLolData(username, my_region):
-    api_key = 'RGAPI-01b2711e-c89e-4473-971d-194af2794ca9'
+    api_key = 'RGAPI-bc79e89e-b443-442a-9620-846d5802f996'
     watcher = LolWatcher(api_key)
     me = watcher.summoner.by_name(my_region, username)
     my_matches = watcher.match.matchlist_by_account(my_region, me['accountId'])
@@ -18,20 +18,31 @@ def getLolData(username, my_region):
     champ_dict = {}
     item_dict = {}
     spell_dict = {}
-    for key in static_champ_list['data']:
-        row = static_champ_list['data'][key]
-        champ_dict[row['key']] = row['id']
+
+    url = 'http://ddragon.leagueoflegends.com/cdn/10.21.1/img/champion/'
+
+    for valueOne in static_champ_list['data']:
+        row = static_champ_list['data'][valueOne]
+        key = row["key"]
+        name = row["name"]
+        image = str(url + str(row["image"]["full"]))
+        value = [name,image]
+        champ_dict[key] = value
+
     for key in static_item_list['data']:
         row = static_item_list['data'][key]
         item_dict[key] = row["name"]
-    for key in static_spell_list['data']:
-        row = static_spell_list['data'][key]
-        spell_dict[key] = row["name"]
+
+    for id in static_spell_list['data']:
+        row = static_spell_list['data'][id]
+        spell_dict[row["key"]] = row["name"]
+
     last_five_match = {}
     for row in my_matches['matches'][:5]:
         key = (row["gameId"])
         value = row["champion"]
         last_five_match[key] = value
+
     players = []
     for key in last_five_match:
         match_detail = (watcher.match.by_id(my_region, key))
@@ -41,8 +52,8 @@ def getLolData(username, my_region):
                 participants_row = {}
                 participants_row['champion'] = row['championId']
                 participants_row['championName'] = champ_dict[str(row['championId'])]
-                participants_row['spell1'] = row['spell1Id']
-                participants_row['spell2'] = row['spell2Id']
+                participants_row['spell1'] = spell_dict[str(row['spell1Id'])]
+                participants_row['spell2'] = spell_dict[str(row['spell2Id'])]
                 participants_row['win'] = row['stats']['win']
                 participants_row['kills'] = row['stats']['kills']
                 participants_row['deaths'] = row['stats']['deaths']
@@ -51,8 +62,11 @@ def getLolData(username, my_region):
                 participants_row['goldEarned'] = row['stats']['goldEarned']
                 participants_row['champLevel'] = row['stats']['champLevel']
                 participants_row['totalMinionsKilled'] = row['stats']['totalMinionsKilled']
-                participants_row['item0'] = row['stats']['item0']
-                participants_row['item1'] = row['stats']['item1']
+                participants_row['item0'] = item_dict[str(row['stats']['item0'])]
+                participants_row['item1'] = item_dict[str(row['stats']['item1'])]
+                participants_row['item2'] = item_dict[str(row['stats']['item2'])]
+                participants_row['item3'] = item_dict[str(row['stats']['item3'])]
+                participants_row['item4'] = item_dict[str(row['stats']['item4'])]
                 players.append(participants_row)
     print(players)
     df = pd.DataFrame(players)

9c3acbe833327ed230b7c22ed2d105cc4249952d
DeVante <DeVantef@buffalo.edu>
2020-11-06 14:04:59 -0500

    added function to render the chart page

--- src/server.py
+++ src/server.py
@@ -9,13 +9,15 @@ UI Code
 """
 
 
-@app.route("/index.html")
+@app.route("/")
 def returnhome():
     return render_template("index.html")
+
 
 @app.route("/league1.html")
 def leagueOne():
     return render_template("league1.html")
+
 
 @app.route("/league1.html", methods=['POST'])
 def league():
@@ -27,38 +29,25 @@ def league():
     except Exception as e:
         return render_template("error.html")
 
+
 @app.route("/cod1.html")
 def codOne():
     return render_template("cod1.html")
 
+
 @app.route("/codStats.html")
 def codStats():
     return render_template("codStats.html")
+
 
 @app.route("/error.html")
 def error():
     return render_template("error.html")
 
 
-@app.route("/")
-def render_chart():
-    return render_template("barChart.html")
-
-@app.route("/bar")
-def render_bar():
-    return render_template("barChart.html")
-
-@app.route("/line")
-def render_line():
-    return render_template("lineChart.html")
-
-@app.route("/radar")
-def render_radar():
-    return render_template("radarChart.html")
-
-@app.route("/pie")
-def render_pie():
-    return render_template("pieChart.html")
+@app.route("/charts")
+def render_charts():
+    return render_template("charts.html")
 
 
 if __name__ == "__main__":

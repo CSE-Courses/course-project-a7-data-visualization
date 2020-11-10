2724a0ccd266a864b07e3bfe4a22333eefce1ada
DeVante <DeVantef@buffalo.edu>
2020-11-10 17:38:06 -0500

    #44 #42 test suites for api call to stats and visuals

--- test/util.test.js
+++ test/util.test.js
@@ -1,6 +1,7 @@
 //test suit using jest, refer to readme file
 
-const { makeApiCallCodTest: makeApiCallCodTEST } = require("/static/codApiCalls.js");
+const { makeApiCallCodTest: makeApiCallCodTEST } = require("/Users/DeVante/IdeaProjects/course-project-a7-data-visualization/src/static/codApiCalls.js");
+const { checkMultipleMatches: testMultiMatch} = require("/Users/DeVante/IdeaProjects/course-project-a7-data-visualization/src/static/mutipleMatchTest.js");
 
 test('API is called and request is sent',()=>{
     const value = makeApiCallCodTEST("curruption123456","warzone","psn");
@@ -25,6 +26,17 @@ test('API is called and request is sent',()=>{
     //invalid platform
     expect(value).toBe(true);
 });
+
+test('checks to make sure multiple match data is ',()=> {
+    const value = testMultiMatch();
+    expect(Object.keys(value).length === 0).toBe(false);
+});
+
+test('check to see back end visuals are linked to the front end',()=>{
+    const value = makeApiCallCodTEST("curruption123456","warzone","psn");
+    //valid username, gamemode, platform
+    expect(value).toBe(true);
+})
 
 //API call should be sent regardless of the validity of the credentials given
 

2b208110918584a01eeeb73e70fa4376a8226ff4
DeVante <DeVantef@buffalo.edu>
2020-12-01 14:52:27 -0500

    #73 bar chart can now display 2 players stats for Warzone battle Royal

--- src/static/charts.js
+++ src/static/charts.js
@@ -1,112 +1,250 @@
-let matchStats;
-let brStats;
+let player1MatchStats;
+let player2MatchStats;
+let player1BrStats;
+let player2BrStats;
 let player1name;
 let player2name;
 
-function renderCharts(){
-    matchStats = JSON.parse(sessionStorage.getItem('matchStats'));
-    brStats = JSON.parse(sessionStorage.getItem('brStats'));
-    player1name = sessionStorage.getItem('gamertag');
-    //player1name = sessionStorage.getItem('gamertag_2');
 
-    console.log(matchStats);
-    renderBar();
-    renderLine();
-    //renderPie();
-    renderRadar();
+function renderCharts(){
+    player1MatchStats = JSON.parse(sessionStorage.getItem('matchStats'));
+    player2MatchStats = JSON.parse(sessionStorage.getItem('matchStats2'));
+    player1BrStats = JSON.parse(sessionStorage.getItem('brStats1'));
+    player2BrStats = JSON.parse(sessionStorage.getItem('brStats2'));
+    player1name = sessionStorage.getItem('gamertag');
+    player2name = sessionStorage.getItem('gamertag_2');
+
+    console.log(player1name)
+    console.log(player2name)
+    console.log(player1BrStats)
+    console.log(player2BrStats)
+    console.log(player1MatchStats)
+    console.log(player2MatchStats)
+
+    if(player1name !== null && player2name !== null){
+        renderBar();
+        renderLine();
+        renderRadar();
+        renderPie();
+    }
+    else{
+        renderBar();
+        renderLine();
+        //renderPie();
+        renderRadar();
+    }
 }
 
 function renderBar(){
     let myChart = document.getElementById('barChart').getContext('2d');
-    let game1kills = matchStats["matches"][0]["playerStats"]["kills"];
-    let game2kills = matchStats["matches"][1]["playerStats"]["kills"];
-    let game3kills = matchStats["matches"][2]["playerStats"]["kills"];
-    let game4kills = matchStats["matches"][3]["playerStats"]["kills"];
-    let game5kills = matchStats["matches"][4]["playerStats"]["kills"];
+    let p1Game1kills = player1MatchStats["matches"]["0"]["playerStats"]["kills"];
+    let p1Game2kills = player1MatchStats["matches"]["1"]["playerStats"]["kills"];
+    let p1Game3kills = player1MatchStats["matches"]["2"]["playerStats"]["kills"];
+    let p1Game4kills = player1MatchStats["matches"]["3"]["playerStats"]["kills"];
+    let p1Game5kills = player1MatchStats["matches"]["4"]["playerStats"]["kills"];
 
-    let maxValue = game1kills;
-    if(maxValue < game2kills){
-        maxValue = game2kills;
+    let p2Game1kills;
+    let p2Game2kills;
+    let p2Game3kills;
+    let p2Game4kills;
+    let p2Game5kills;
+
+    if(player2name !== null){
+        p2Game1kills = player2MatchStats["matches"]["0"]["playerStats"]["kills"];
+        p2Game2kills = player2MatchStats["matches"]["1"]["playerStats"]["kills"];
+        p2Game3kills = player2MatchStats["matches"]["2"]["playerStats"]["kills"];
+        p2Game4kills = player2MatchStats["matches"]["3"]["playerStats"]["kills"];
+        p2Game5kills = player2MatchStats["matches"]["4"]["playerStats"]["kills"];
     }
-    if(maxValue < game3kills){
-        maxValue = game3kills;
+
+    let maxValue = p1Game1kills;
+    if(maxValue < p1Game2kills){
+        maxValue = p1Game2kills;
     }
-    if(maxValue < game4kills){
-        maxValue = game4kills;
+    if(maxValue < p1Game3kills){
+        maxValue = p1Game3kills;
     }
-    if(maxValue < game5kills){
-        maxValue = game5kills;
+    if(maxValue < p1Game4kills){
+        maxValue = p1Game4kills;
+    }
+    if(maxValue < p1Game5kills){
+        maxValue = p1Game5kills;
+    }
+
+    if(player2name != null){
+        if(maxValue < p2Game2kills){
+            maxValue = p2Game2kills;
+        }
+        if(maxValue < p2Game3kills){
+            maxValue = p2Game3kills;
+        }
+        if(maxValue < p2Game4kills){
+            maxValue = p2Game4kills;
+        }
+        if(maxValue < p2Game5kills){
+            maxValue = p2Game5kills;
+        }
     }
     //global options
-    let massPopChart = new Chart(myChart,{
-        type: 'bar', //bar, horozontalbar, pie, line, doughnut
-        data:{
-            labels:["game 1","game 2","game 3","game 4","game 5"],
-            datasets:[{
-                barPercentage:0.5,
-                categoryPercentage:0.7,
-                data:[game1kills,game2kills,game3kills,game4kills,game5kills],
-                //backgroundColor:'green'
-                backgroundColor:[
-                    'rgb(17,78,3)',
-                    'rgb(17,78,3)',
-                    'rgb(17,78,3)',
-                    'rgb(17,78,3)',
-                    'rgb(17,78,3)'
-                ],
-                borderWidth:2,
-                borderColor:"#020202",
-                barThickness: 60,
-                hoverBorderColor: "#ffd500",
-                hoverBorderWidth:3
-            }],
-        },
-        options:{
-            title: { // floating title to be specific
-                display: true,
-                text: 'Kills per game',
-                titleFontSize: 8
-            },
-            legend:{
-                position: "right",
-                display: false
-            },
-            layout: {
-                padding: {
-                    left: 50,
-                    right: 50,
-                    top: 50,
-                    bottom: 50
-                }
-            },
-            tooltips:{
-                titleFontSize: 14
-
-            },
-            scales: {
-                yAxes: [{
-                    scaleLabel: {
-                        display: true,
-                        labelString: "kills"
-                    },
-                    ticks: {
-                        beginAtZero: true,
-                        suggestedMax: maxValue + 2,
-                        stepSize: 1
-                    }
+    if(player2name != null) {
+        let massPopChart = new Chart(myChart, {
+            type: 'bar', //bar, horozontalbar, pie, line, doughnut
+            data: {
+                labels: ["game 1", "game 2", "game 3", "game 4", "game 5"],
+                datasets: [{
+                    barPercentage: 0.5,
+                    categoryPercentage: 0.7,
+                    data: [p1Game1kills, p1Game2kills, p1Game3kills, p1Game4kills, p1Game5kills],
+                    //backgroundColor:'green'
+                    backgroundColor: [
+                        'rgb(17,78,3)',
+                        'rgb(17,78,3)',
+                        'rgb(17,78,3)',
+                        'rgb(17,78,3)',
+                        'rgb(17,78,3)'
+                    ],
+                    borderWidth: 2,
+                    borderColor: "#020202",
+                    barThickness: 60,
+                    hoverBorderColor: "#ffd500",
+                    hoverBorderWidth: 3
+                }, {
+                    barPercentage: 0.5,
+                    categoryPercentage: 0.7,
+                    data: [p2Game1kills, p2Game2kills, p2Game3kills, p2Game4kills, p2Game5kills],
+                    //backgroundColor:'green'
+                    backgroundColor: [
+                        'rgb(3,19,78)',
+                        'rgb(3,19,78)',
+                        'rgb(3,19,78)',
+                        'rgb(3,19,78)',
+                        'rgb(3,19,78)'
+                    ],
+                    borderWidth: 2,
+                    borderColor: "#020202",
+                    barThickness: 60,
+                    hoverBorderColor: "#ffd500",
+                    hoverBorderWidth: 3
                 }],
-                xAxes: [{
-                    scaleLabel: {
-                        display: true,
-                        //labelString: "games"
-                    },
-                    ticks: {
-                        beginAtZero: true,
-                    },
-                }]
+            },
+            options: {
+                title: { // floating title to be specific
+                    display: true,
+                    text: 'Kills per game',
+                    titleFontSize: 8
+                },
+                legend: {
+                    position: "right",
+                    display: false
+                },
+                layout: {
+                    padding: {
+                        left: 50,
+                        right: 50,
+                        top: 50,
+                        bottom: 50
+                    }
+                },
+                tooltips: {
+                    titleFontSize: 14
+
+                },
+                scales: {
+                    yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: "kills"
+                        },
+                        ticks: {
+                            beginAtZero: true,
+                            suggestedMax: maxValue + 2,
+                            stepSize: 1
+                        }
+                    }],
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            //labelString: "games"
+                        },
+                        ticks: {
+                            beginAtZero: true,
+                        },
+                    }]
+                }
             }
-        }
-    })
+        })
+    }
+    else{
+        let massPopChart = new Chart(myChart, {
+            type: 'bar', //bar, horozontalbar, pie, line, doughnut
+            data: {
+                labels: ["game 1", "game 2", "game 3", "game 4", "game 5"],
+                datasets: [{
+                    barPercentage: 0.5,
+                    categoryPercentage: 0.7,
+                    data: [p1Game1kills, p1Game2kills, p1Game3kills, p1Game4kills, p1Game5kills],
+                    //backgroundColor:'green'
+                    backgroundColor: [
+                        'rgb(17,78,3)',
+                        'rgb(17,78,3)',
+                        'rgb(17,78,3)',
+                        'rgb(17,78,3)',
+                        'rgb(17,78,3)'
+                    ],
+                    borderWidth: 2,
+                    borderColor: "#020202",
+                    barThickness: 60,
+                    hoverBorderColor: "#ffd500",
+                    hoverBorderWidth: 3
+                }],
+            },
+            options: {
+                title: { // floating title to be specific
+                    display: true,
+                    text: 'Kills per game',
+                    titleFontSize: 8
+                },
+                legend: {
+                    position: "right",
+                    display: false
+                },
+                layout: {
+                    padding: {
+                        left: 50,
+                        right: 50,
+                        top: 50,
+                        bottom: 50
+                    }
+                },
+                tooltips: {
+                    titleFontSize: 14
+
+                },
+                scales: {
+                    yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: "kills"
+                        },
+                        ticks: {
+                            beginAtZero: true,
+                            suggestedMax: maxValue + 2,
+                            stepSize: 1
+                        }
+                    }],
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            //labelString: "games"
+                        },
+                        ticks: {
+                            beginAtZero: true,
+                        },
+                    }]
+                }
+            }
+        })
+    }
 }
 
 function renderPie(){
@@ -167,9 +305,9 @@ function renderRadar(){
     Chart.defaults.global.defaultFontFamily = "lato";
     Chart.defaults.global.defaultFontSize = 18;
     Chart.defaults.global.defaultFontColor = "#020202";
-    let kills = brStats["br"]["kills"];
-    let deaths = brStats["br"]["deaths"];
-    let downs = brStats["br"]["downs"];
+    let kills = player1BrStats["br"]["kills"];
+    let deaths = player1BrStats["br"]["deaths"];
+    let downs = player1BrStats["br"]["downs"];
     //let topFives = stats["br"]["topFive"];
     let suggestedMax = kills;
     if(suggestedMax < deaths){
@@ -239,17 +377,17 @@ function renderLine(){
     Chart.defaults.global.defaultFontSize = 18;
     Chart.defaults.global.defaultFontColor = "#020202";
 
-    let damage1 = matchStats["matches"][0]["playerStats"]["damageDone"];
-    let damage2 = matchStats["matches"][1]["playerStats"]["damageDone"];
-    let damage3 = matchStats["matches"][2]["playerStats"]["damageDone"];
-    let damage4 = matchStats["matches"][3]["playerStats"]["damageDone"];
-    let damage5 = matchStats["matches"][4]["playerStats"]["damageDone"];
+    let damage1 = player1MatchStats["matches"][0]["playerStats"]["damageDone"];
+    let damage2 = player1MatchStats["matches"][1]["playerStats"]["damageDone"];
+    let damage3 = player1MatchStats["matches"][2]["playerStats"]["damageDone"];
+    let damage4 = player1MatchStats["matches"][3]["playerStats"]["damageDone"];
+    let damage5 = player1MatchStats["matches"][4]["playerStats"]["damageDone"];
 
-    let kills1 = matchStats["matches"][0]["playerStats"]["damageTaken"];
-    let kills2 = matchStats["matches"][1]["playerStats"]["damageTaken"];
-    let kills3 = matchStats["matches"][2]["playerStats"]["damageTaken"];
-    let kills4 = matchStats["matches"][3]["playerStats"]["damageTaken"];
-    let kills5 = matchStats["matches"][4]["playerStats"]["damageTaken"];
+    let kills1 = player1MatchStats["matches"][0]["playerStats"]["damageTaken"];
+    let kills2 = player1MatchStats["matches"][1]["playerStats"]["damageTaken"];
+    let kills3 = player1MatchStats["matches"][2]["playerStats"]["damageTaken"];
+    let kills4 = player1MatchStats["matches"][3]["playerStats"]["damageTaken"];
+    let kills5 = player1MatchStats["matches"][4]["playerStats"]["damageTaken"];
 
     let massPopChart = new Chart(myChart,{
         type: 'line', //bar, horozontalbar, pie, line, doughnut
